2c90c45dfb4a51b3fed8c7cd0c61296f8d8b1eef
cedrickkouek@gmail.com <cedrickkouek@gmail.com>
2023-04-26 21:40:35 +0000

    yessir

--- _printf.c
+++ _printf.c
@@ -4,7 +4,7 @@
  * @format: It is the identifier to look for.
  * Return: The length of the string.
  */
-int _printf(const char * const format, ...)
+int _printf(const char *format, ...)
 {
 	convert_match m[] = {
 		{"%s", printf_string}, {"%c", printf_char},
